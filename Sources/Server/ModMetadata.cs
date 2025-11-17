4394322680964554bc2774428f4c905d8db5a7b1
SwiftXP <230956466+swiftxp-hub@users.noreply.github.com>
2025-11-17 23:30:01 +0100

    Added SPT 4.0.5 as requirement.

--- Sources/Server/ModMetadata.cs
+++ Sources/Server/ModMetadata.cs
@@ -10,7 +10,7 @@ public record ModMetadata : AbstractModMetadata
     public override string Author { get; init; } = "SwiftXP";
     public override List<string>? Contributors { get; init; } = [];
     public override SemanticVersioning.Version Version { get; init; } = new("2.5.1");
-    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.2");
+    public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.5");
 
     public override List<string>? Incompatibilities { get; init; } = [];
     public override Dictionary<string, SemanticVersioning.Range>? ModDependencies { get; init; } = [];
